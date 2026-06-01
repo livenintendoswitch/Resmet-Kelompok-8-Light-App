836865ce134d043eb933f86da97d8933df44e880
Leonard <leonardsuwanta3@gmail.com>
2026-06-01 22:55:21 +0700

    azure init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:3001'
